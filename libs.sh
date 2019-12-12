e10ab7be72384c132ad86a3e6c476afa85a407f9
Carlos M. Rivera <ingcarlosmrivera@gmail.com>
2019-12-12 15:58:33 -0300

    \r problem on libs.sh

--- libs.sh
+++ libs.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e
-
 VERSION="v2.8.0"
 URL="https://github.com/datso/react-native-pjsip-builder/releases/download/${VERSION}/release.tar.gz"
 LOCK=".libs.lock"
 DEST=".libs.tar.gz"
 DOWNLOAD=true
-
 if ! type "curl" > /dev/null; then
     echo "Missed curl dependency" >&2;
     exit 1;
@@ -15,7 +13,6 @@ if ! type "tar" > /dev/null; then
     echo "Missed tar dependency" >&2;
     exit 1;
 fi
-
 if [ -f ${LOCK} ]; then
     CURRENT_VERSION=$(cat ${LOCK})
 
@@ -23,7 +20,6 @@ if [ -f ${LOCK} ]; then
         DOWNLOAD=false
     fi
 fi
-
 if [ "$DOWNLOAD" = true ]; then
     curl -L --silent "${URL}" -o "${DEST}"
     tar -xvf "${DEST}"
