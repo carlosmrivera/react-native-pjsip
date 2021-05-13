6666d90abf9f4216f790f28efa10643bafa54e31
Carlos M. Rivera <ingcarlosmrivera@gmail.com>
2021-05-13 19:24:38 +0200

    updated v2.8.1

--- libs.sh
+++ libs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # comment
 set  -e
-VERSION="v2.8.0"
+VERSION="v2.8.1"
 URL="https://github.com/ingcarlosmrivera/react-native-pjsip-builder/releases/download/${VERSION}/release.tar.gz"
 LOCK=".libs.lock"
 DEST=".libs.tar.gz"
